9d805ed05b9aedc9b9f990b3de9734f1e0b714ff
Yehor Smoliakov <yehors+gh@ukr.net>
2023-06-16 23:44:15 +0300

    A fix

--- inference_mel_radtts.py
+++ inference_mel_radtts.py
@@ -8,19 +8,19 @@ from vocos import Vocos
 
 model = Vocos.from_hparams("my_config.yaml")
 
-raw_model = torch.load('/home/yehor/Work/github/vocos/logs/lightning_logs/version_1/checkpoints/vocos_checkpoint_epoch=336_step=214332_val_loss=10.2031.ckpt', map_location="cuda")
+model_path = '/home/yehor/Work/github/vocos/logs/lightning_logs/version_1/checkpoints/vocos_checkpoint_epoch=336_step=214014_val_loss=10.2133.ckpt'
+
+raw_model = torch.load(model_path, map_location="cpu")
 
 model.load_state_dict(raw_model['state_dict'], strict=False)
 model.eval()
-
-model.to('cuda')
 
 for mel_file in glob('tetiana_samples/*.mel'):
     ts_start = time.time()
 
     wav_file = mel_file.replace('.mel', '.wav')
 
-    mel_1 = torch.load(mel_file)
+    mel_1 = torch.load(mel_file).to('cpu')
     audio = model.decode(mel_1)
 
     ts_end = time.time() -  ts_start
